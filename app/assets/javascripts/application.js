a421ed919385f2b6ad5c7b55a80f5852d71ddfbb
Julio Noguera <noguerajulio24@gmail.com>
2017-11-01 11:58:52 -0300

    comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,12 @@
 
   $(document).ready(function() {
     $('select').material_select();
-  
+ });
 
-
-
+$(document).ready(function() {
   // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   $('.collapsible').collapsible();
+});
 
- });
